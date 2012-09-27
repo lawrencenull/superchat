3e21e4c7ad0a7554f05b7b4bea5a8ca93ec19bb8
Michael Puckett <michael.puckett@iostudio.com>
2012-09-27 17:50:19 -0500

    ready to rock

---
+++ app.js
@@ -0,0 +1,53 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , socket_io = require('socket.io').listen(8080);
+
+/**
+ * Express setup
+ */
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'html');
+  app.engine('html', require('ejs').__express);
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+/**
+  Socket.io setup
+  */
+
+socket_io.sockets.on('connection', function (socket) {
+  console.log('connected baby');
+  socket.on('newFile', function (data) {
+    console.log(data);
+    socket_io.sockets.emit('_sendFile', data );
+  });
+});
