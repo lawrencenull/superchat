be94b1213c215164f8bc216e31a58e0183e16bd8
Michael Puckett <michael.puckett@iostudio.com>
2012-09-27 19:24:58 -0500

    backbone on server

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var express = require('express')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path')
-  , socket_io = require('socket.io').listen(8080);
+  , socket_io = require('socket.io').listen(8080)
+  , Backbone = require('backbone');
 
 /**
  * Express setup
@@ -41,13 +42,84 @@ http.createServer(app).listen(app.get('port'), function(){
 });
 
 /**
+  Backbone setup
+  */
+
+var UserModel = Backbone.Model.extend({
+  defaults: {
+    'name': 'New User'
+  },
+  initialize: function () {
+    if (this.get('name') === this.defaults.name) {
+      this.set('name', 'anon_' + this.id);
+    }
+    this.on('add', function (user) {
+      appController.trigger('newUserAdded', user);
+    });
+  }
+});
+
+var UsersCollection = Backbone.Collection.extend({
+  model: UserModel
+});
+
+// identical model on client
+
+var FileModel = Backbone.Model.extend({
+    defaults: {
+        'name': 'file.txt',
+        'data': 'data:',
+        'author': 'New Author',
+        'timestamp': 'New Date',
+        'location': {
+            'offsetX': 0,
+            'offsetY': 0
+        }
+    }
+});
+
+var FilesCollection = Backbone.Collection.extend({
+    model: FileModel,
+    initialize: function () {
+        this.on('add', function (file) {
+            appController.trigger('newFileAdded', file);
+        });
+    }
+});
+
+var AppController = Backbone.Model.extend({
+  initialize: function () {
+    var filesCollection = new FilesCollection();
+    var usersCollection = new UsersCollection();
+    this.on('newUser', function (user) {
+      usersCollection.add({id: user.id });
+      user.emit( '_getAllFiles', filesCollection.toJSON() );
+      user.emit( '_getAllUsers', usersCollection.toJSON() );
+    });
+    this.on('newFileFromClient', function (file) {
+      filesCollection.add(file);
+    });
+    this.on('newFileAdded', function (file) {
+      socket_io.sockets.emit( '_newFile', file.toJSON() );
+    });
+    this.on('newUserAdded', function (user) {
+      var socketid = user.id;
+      socket_io.sockets.socket(socketid).emit( '_modifyCurrentUser', user);
+      socket_io.sockets.except(socketid).emit( '_newUser', user);
+    });
+  }
+});
+
+var appController = new AppController();
+
+
+/**
   Socket.io setup
   */
 
 socket_io.sockets.on('connection', function (socket) {
-  console.log('connected baby');
+  appController.trigger('newUser', socket);
   socket.on('newFile', function (data) {
-    console.log(data);
-    socket_io.sockets.emit('_sendFile', data );
+    appController.trigger('newFileFromClient', data);
   });
 });
