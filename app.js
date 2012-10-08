f5d396b2d88de947773c9436e52924e854f4c181
Michael Puckett <michaelcpuckett@gmail.com>
2012-10-08 11:51:07 -0500

    cross domain

--- app.js
+++ app.js
@@ -30,6 +30,14 @@ var express = require('express'),
  * Express setup
  */
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+};
+
 
 var app = express();
 
@@ -42,6 +50,7 @@ app.configure(function () {
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
+    app.use(allowCrossDomain);
     app.use(app.router);
     app.use(express.static(path.join(__dirname, 'public')));
 });
