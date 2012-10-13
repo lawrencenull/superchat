2b97f5b77954721136c25a4195d1a87e32f9df3b
Michael Puckett <mcpuck@menkent.dreamhost.com>
2012-10-13 11:34:39 -0700

    trolo tweaking with bill

--- trolo.js
+++ trolo.js
@@ -10,11 +10,12 @@ var transcription = {"id":"1234", "url":"mailto:michaelcpuckett@.com"};
 var say = tropo.say("Hello, how are you?.", null, null, null, "victor");
 var choices = new Choices(null,null,'#');
 
-tropo.record(null, null, null, choices, null, 7, 60, null, null, "recording", null, say, 10, transcription, "ftp://mcpuck:Agent106!:ftp.pickpuck.com/recordings.js");
+//var say = tropo.say("Oh my dear friend Bill. How are you?");
+
+//tropo.record(null, null, null, choices, null, 7, 60, null, null, "recording", null, say, 10, transcription, "ftp://mcpuck:Agent106!:ftp.pickpuck.com/recordings.js");
 
 // function (attempts, bargein, beep, choices, format, maxSilence, maxTime, method, minConfidence, name, required, say, timeout, transcription, url, password, username)
-
-//tropo.record(null, null, null, choices, null, null, null, null, null, "recording", null, say, null, null, "ftp://ftp.pickpuck.com/pickpuck.com/recording.wav", 'mcpuck', 'Agent106!');
+tropo.record(null, null, null, choices, null, null, null, null, null, "recording", null, say, null, null, "ftp://mcpuck:Agent106!@ftp.pickpuck.com/pickpuck.com/recording.wav");
 
 //tropo.record(null, null, null, choices, "audio/mp3", 7, 60, null, null, "recording", null, say, 10, {url:'michaelcpuckett@gmail.com'}, "ftp://mcpuck:Agent106!@ftp.pickpuck.com/pickpuck.com/recording.mp3");
      
