84c54762de159e03111181337e897e428a23a45d
Michael Puckett <michaelcpuckett@gmail.com>
2012-10-13 14:15:03 -0500

    password and username at the end

--- trolo.js
+++ trolo.js
@@ -14,12 +14,16 @@ var choices = new Choices(null,null,'#');
 
 //tropo.record(null, null, null, choices, null, 7, 60, null, null, "recording", null, say, 10, transcription, "ftp://mcpuck:Agent106!:ftp.pickpuck.com/recordings.js");
 
-// function (attempts, bargein, beep, choices, format, maxSilence, maxTime, method, minConfidence, name, required, say, timeout, transcription, url, password, username)
 //tropo.record(null, null, null, choices, null, null, null, null, null, "recording", null, say, null, null, "ftp://mcpuck:Agent106!@ftp.pickpuck.com/pickpuck.com/recording.wav");
 
-//tropo.record(null, null, null, true, choices, say, "audio/mp3", 7.0, 140.0, null, null, "recording", null, transcription, "ftp://mcpuck:Agent106!@ftp.pickpuck.com/pickpuck.com/recording.mp3");
+
+//(attempts, bargein, beep, choices, format, maxSilence, maxTime, method, minConfidence, name, required, say, timeout, transcription, url, password, username) {
+    
+tropo.record(null, null, true, choices, null, 7.0, 120.0, null, null, "recording", null, say, 10.0, null, "ftp://ftp.pickpuck.com/pickpuck.com/recording.mp3", "Agent106!", "mcpuck");
      
-tropo.record(3, false, null, choices, "audio/wav", 5, 120, null, null, "recording", null, say, 5, null, "ftp://mcpuck:Agent106!@ftp.pickpuck.com/pickpuck.com/sandbox/iostudio-whiteboard/recording.mp3");
+// tropo.record(3, false, null, choices, "audio/wav", 5, 120, null, null, "recording", null, say, 5, null, "ftp://mcpuck:Agent106!@ftp.pickpuck.com/pickpuck.com/sandbox/iostudio-whiteboard/recording.mp3");
+
+
 
 response.end(TropoJSON(tropo));
 
