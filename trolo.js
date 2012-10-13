a5795902659a937fd0c02e9da1c46ae512a06d6d
Michael Puckett <mcpuck@menkent.dreamhost.com>
2012-10-13 02:01:23 -0700

    trolo

--- trolo.js
+++ trolo.js
@@ -4,9 +4,10 @@ var tropo_webapi = require('tropo-webapi');
 var server = http.createServer(function (request, response) {
      
     var tropo = new TropoWebAPI();
- 
-    tropo.say("Welcome to Tropo!");
+ tropo.say("http://www.phono.com/audio/troporocks.mp3 Here's some hold music! http://www.phono.com/audio/holdmusic.mp3");
+     
+//    tropo.say("My name is Michael Puckett!");
      
     response.end(TropoJSON(tropo));
  
-}).listen(8000); 
+}).listen(3000); 
