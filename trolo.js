2a5f349f690ae450c37bdd25418207053176c2d6
Michael Puckett <michaelcpuckett@gmail.com>
2012-10-13 13:41:31 -0500

    bill is awesome for helping me

--- trolo.js
+++ trolo.js
@@ -15,9 +15,31 @@ var choices = new Choices(null,null,'#');
 //tropo.record(null, null, null, choices, null, 7, 60, null, null, "recording", null, say, 10, transcription, "ftp://mcpuck:Agent106!:ftp.pickpuck.com/recordings.js");
 
 // function (attempts, bargein, beep, choices, format, maxSilence, maxTime, method, minConfidence, name, required, say, timeout, transcription, url, password, username)
-tropo.record(null, null, null, choices, null, null, null, null, null, "recording", null, say, null, null, "ftp://mcpuck:Agent106!@ftp.pickpuck.com/pickpuck.com/recording.wav");
+//tropo.record(null, null, null, choices, null, null, null, null, null, "recording", null, say, null, null, "ftp://mcpuck:Agent106!@ftp.pickpuck.com/pickpuck.com/recording.wav");
 
-//tropo.record(null, null, null, choices, "audio/mp3", 7, 60, null, null, "recording", null, say, 10, {url:'michaelcpuckett@gmail.com'}, "ftp://mcpuck:Agent106!@ftp.pickpuck.com/pickpuck.com/recording.mp3");
+{ "record": {
+    "attempts": Integer,
+    "allowSignals": String or Array,
+    "bargein": Boolean,
+    "beep": Boolean,
+    "choices": Object,
+    "say": Object,
+    "format": String,
+    "maxSilence": Float,
+    "maxTime": Float,
+    "method": String,
+    "minConfidence": Integer,
+    "name": String,
+    "required": Boolean,
+    "transcription": Array or Object,
+    "url": String,
+    "password": String,
+    "username": String,
+    "timeout": Float,
+    "interdigitTimeout": Integer,
+    "voice": String } } 
+
+tropo.record(null, null, null, true, choices, say, "audio/mp3", 7.0, 140.0, null, null, "recording", null, transcription, "ftp://mcpuck:Agent106!@ftp.pickpuck.com/pickpuck.com/recording.mp3");
      
 
 response.end(TropoJSON(tropo));
