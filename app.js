98b13ef0e1ecc2c0de0da6e88b2616b63527cf46
Michael Puckett <michaelcpuckett@gmail.com>
2012-10-13 18:31:02 -0500

    phone

--- app.js
+++ app.js
@@ -71,9 +71,7 @@ var choices = new Choices(null,null,'#')
 
 tropo.record(null, null, true, choices, null, 7.0, 120.0, null, null, "recording", null, say, 10.0, transcription, "ftp://ftp.pickpuck.com/pickpuck.com/recording.mp3", "Agent106!", "mcpuck");
 
-    var session = JSON.parse(json);
-    var callerID = session.session.from.id;
-    console.log(callerID);
+console.log(req);
 
 res.end(TropoJSON(tropo));
 
