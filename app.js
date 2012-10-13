f399d3cf777fd6ade75440d4d20cdc1e0f79b4aa
Michael Puckett <michaelcpuckett@gmail.com>
2012-10-13 18:42:10 -0500

    socket

--- app.js
+++ app.js
@@ -227,7 +227,8 @@ appController = new AppController();
   */
 
 app.post('/call', function (req, res, next) { 
-    console.log(req.body.result); 
+    console.log(req.body.result);
+    console.log('user', req.body.result.identifier);
     appController.trigger('_chatMessageAdded', {user: { id: req.body.result.identifier }, message:req.body.result.transcription}); 
     res.write('', 200); 
 });
