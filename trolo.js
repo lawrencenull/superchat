37fe1869b3ebcaa57188b51712c9052e90d65013
Michael Puckett <michaelcpuckett@gmail.com>
2012-10-13 01:44:07 -0500

    trolo

--- trolo.js
+++ trolo.js
@@ -1,50 +1,46 @@
-var http = require('http');
-var tropowebapi = require('tropo-webapi');
- 
-// http://en.wikipedia.org/wiki/Magic_8-ball
-var answers = [
-"It is certain",
-"It is decidedly so",
-"Without a doubt",
-"Yes – definitely",
-"You may rely on it",
-"As I see it, yes",
-"Most likely",
-"Outlook good",
-"Signs point to yes",
-"Yes",
-"Reply hazy, try again",
-"Ask again later",
-"Better not tell you now",
-"Cannot predict now",
-"Concentrate and ask again",
-"Don't count on it",
-"My reply is no",
-"My sources say no",
-"Outlook not so good",
-"Very doubtful"
-];
- 
-http.createServer(function (req, res) {
- 
-    // Create a new instance of the TropoWebAPI object.
-    var tropo = new tropowebapi.TropoWebAPI();  
-    var answer = answers[Math.floor(Math.random()*answers.length)];
- 
-    tropo.say("Welcome to the tropo node J S magic 8 ball.");
- 
-    var say = new Say("Ask your question now after the beep.");
-    var choices = new Choices(null, null, "#");
- 
-    // Action classes can be passed as parameters to TropoWebAPI class methods.
-    // use the record method https://www.tropo.com/docs/webapi/record.htm
-    tropo.record(3, false, null, choices, "audio/wav", 5, 60, null, null, "recording", null, say, 5, null, "http://example.com/tropo", null, null);
- 
-    tropo.say(answer);
- 
-    tropo.say("Please call back to play again!");
- 
-    res.writeHead(200, {'Content-Type': 'application/json'}); 
-    res.end(tropowebapi.TropoJSON(tropo));
- 
-}).listen(13188);
+/**
+ * Showing with the Express framwork http://expressjs.com/
+ * Express must be installed for this sample to work
+ */
+ 
+require('../../lib/tropo-webapi');
+var express = require('express');
+var app = express.createServer();
+ 
+/**
+ * Required to process the HTTP body
+ * req.body has the Object while req.rawBody has the JSON string
+ */
+app.configure(function(){
+    app.use(express.bodyDecoder());
+});
+ 
+app.post('/', function(req, res){
+    // Create a new instance of the TropoWebAPI object.
+    var tropo = new TropoWebAPI();
+    // Use the say method https://www.tropo.com/docs/webapi/say.htm
+    tropo.say("Welcome to my Tropo Web API node demo.");
+ 
+    // Demonstrates how to use the base Tropo action classes.
+    var say = new Say("Please enter your 5 digit zip code.");
+    var choices = new Choices("[5 DIGITS]");
+ 
+    // Action classes can be passes as parameters to TropoWebAPI class methods.
+    // use the ask method https://www.tropo.com/docs/webapi/ask.htm
+    tropo.ask(choices, 3, false, null, "foo", null, true, say, 5, null);
+    // use the on method https://www.tropo.com/docs/webapi/on.htm
+    tropo.on("continue", null, "/answer", true);
+ 
+    res.send(TropoJSON(tropo));
+});
+ 
+app.post('/answer', function(req, res){
+    // Create a new instance of the TropoWebAPI object.
+    var tropo = new TropoWebAPI();
+    tropo.say("Your zip code is " + req.body['result']['actions']['interpretation']);
+ 
+    res.send(TropoJSON(tropo));
+});
+ 
+app.listen(8000);
+//console.log('Server running on http://0.0.0.0:8000/')
