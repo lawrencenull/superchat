08277ed2052b7ac3f0b6003f89ca3318697e4e69
Michael Puckett <michaelcpuckett@gmail.com>
2012-10-13 22:58:09 -0500

    trans

--- app.js
+++ app.js
@@ -174,7 +174,7 @@ app.post('/record', function (req,res) {
 
     var transcription = {"id":phoneNumber, "url":"http://54.243.182.246:3000/call"};
     var choices = new Choices(null,null,'#')
-    tropo.record(null, null, true, choices, null, 7.0, 120.0, null, null, "recording", null, say, 10.0, null, "ftp://ftp.pickpuck.com/pickpuck.com/recording.mp3", "Agent106!", "mcpuck");
+    tropo.record(null, null, true, choices, null, 7.0, 120.0, null, null, "recording", null, say, 10.0, transcription, "ftp://ftp.pickpuck.com/pickpuck.com/recording.mp3", "Agent106!", "mcpuck");
 
     tropo.on('continue', null, '/messages?id='+phoneNumber, true);
 
